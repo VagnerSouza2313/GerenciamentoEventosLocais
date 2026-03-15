552485cc901cf4335b836220a547191ac8d1a113
Vagner Souza <juninfps0@gmail.com>
2026-03-15 14:46:32 -0300

    Feita entity de evento

--- src/main/java/com/vagner/EventClean/infra/persistence/EventEntity.java
+++ src/main/java/com/vagner/EventClean/infra/persistence/EventEntity.java
@@ -1,4 +1,33 @@
 package com.vagner.EventClean.infra.persistence;
 
+import com.vagner.EventClean.core.enuns.TipoEvento;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+@Table(name = "Eventos")
 public class EventEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String descricao;
+    private LocalDateTime data_inicio;
+    private LocalDateTime data_fim;
+    private String identificar;
+    private String local_evento;
+    private Integer capacidade;
+    private String organizador;
+    @Enumerated(EnumType.STRING)
+    private TipoEvento tipo;
+
 }
