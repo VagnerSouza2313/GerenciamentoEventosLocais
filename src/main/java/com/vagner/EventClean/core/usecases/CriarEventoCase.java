b1ee5f86aa5360b6a40a4b378039f98fefa39e12
Vagner Souza <juninfps0@gmail.com>
2026-04-05 00:40:57 -0300

    refactor: padrão UpperCamelCase do nome da interface

--- src/main/java/com/vagner/EventClean/core/usecases/CriarEventoCase.java
+++ src/main/java/com/vagner/EventClean/core/usecases/CriarEventoCase.java
@@ -2,7 +2,7 @@ package com.vagner.EventClean.core.usecases;
 
 import com.vagner.EventClean.core.entities.Event;
 
-public interface criarEventoCase {
+public interface CriarEventoCase {
 
     //Criar evento
     public Event execute(Event evento);
